289e4f01efe01b5d4c6b7e498391cfbac8f29301
Paul Campbell <Paul.Campbell@bas.local>
2019-09-08 00:32:56 +0100

    TP6 missed the refactoring in the server.go file and removed the handle function callbacks from there

--- server.go
+++ server.go
@@ -3,16 +3,12 @@ package main
 import (
     "log"
     "net/http"
-	"github.com/gorilla/mux"
 )
  
 func main() {
 	encryptionMap = make(map[string][]byte)
 	
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/encrypt", Encrypt)
-    router.HandleFunc("/decrypt", Decrypt)
- 
+	router := NewRouter()
     log.Fatal(http.ListenAndServe(":8080", router))
 }
  
