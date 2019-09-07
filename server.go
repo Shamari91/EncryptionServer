557e1022668ecfea8e464608125b20c219de2c98
Paul Campbell <Paul.Campbell@bas.local>
2019-09-08 00:27:59 +0100

    TP6 REFACTORING! moved all the handlers into handler.go and placed all the routes into routes.go

--- server.go
+++ server.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-    "fmt"
     "log"
     "net/http"
-    "io/ioutil"
 	"github.com/gorilla/mux"
-	"encoding/json"
 )
  
 func main() {
@@ -17,107 +14,5 @@ func main() {
     router.HandleFunc("/decrypt", Decrypt)
  
     log.Fatal(http.ListenAndServe(":8080", router))
-}
- 
-type encryptionReq struct {
-  ID string
-  Data string
-}
-
-type encryptionResponse struct {
-	Result string
-	Key []byte 
-}
-
-func Encrypt(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Encrypting!")
-	
-	body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-      http.Error(w, "can't read body", http.StatusBadRequest)
-      return
-	}
-
-	var requestData encryptionReq
-	err = json.Unmarshal(body, &requestData)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	responseData := encryptionResponse{}
-    if requestData.ID == "" {
-		responseData.Result = "ID parameter is missing!"
-	} else {
-		dataBytes := []byte(requestData.Data)
-		encryptedData, key, err := encrypt(dataBytes)
-		if err != nil {
-			log.Printf("Error when encrypting: %v", err)
-			http.Error(w, err.Error(), 500)
-			return
-		}
-
-		log.Printf("Encrypted data: %v, Key Is: %v", encryptedData, key)
-
-		responseData.Result = "Data encrypted succesfully!"
-		responseData.Key = key
-
-		persistEncryptionData(requestData.ID, encryptedData)
-	}
-
-	responseBody, err := json.Marshal(responseData)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(responseBody)
-}
-
-type decryptionReq struct {
-	ID  string
-	Key []byte
-}
-
-type decryptionResponse struct {
-	Result string
-	Data   string
-}
-
-func Decrypt(w http.ResponseWriter, r *http.Request) {
-	reqBody, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	var reqPayload decryptionReq
-	err = json.Unmarshal(reqBody, &reqPayload)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	resultPayload := decryptionResponse{}
-	if reqPayload.ID == "" {
-		resultPayload.Result = "ID parameter is missing!"
-	} else if reqPayload.Key == nil {
-		resultPayload.Result = "Key paramter is missing!"
-	} else {
-		encryptedData := retrieveEncryptionData(reqPayload.ID)
-		if encryptedData == nil {
-			resultPayload.Data = ""
-		} else {
-			decryptedData, err := decrypt(encryptedData, reqPayload.Key)
-			if err != nil {
-				fmt.Printf("Error occured while trying to decrypt the data: %v \n", err)
-				resultPayload.Data = ""
-			} else {
-				resultPayload.Data = decryptedData
-			}
-		}
-		resultPayload.Result = "Data decrypted succesfully!"
-	}
-
-	responseBody, err := json.Marshal(resultPayload)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(responseBody)
 }
  
