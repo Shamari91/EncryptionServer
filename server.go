1f5d5de3fd5b1cc295bf481d78230c6e1ce05b64
Paul Campbell <Paul.Campbell@bas.local>
2019-09-07 23:13:27 +0100

    TP2 added code into the server to call the encryption function and send a respose to the user

--- server.go
+++ server.go
@@ -4,8 +4,9 @@ import (
     "fmt"
     "log"
     "net/http"
- 
-    "github.com/gorilla/mux"
+    "io/ioutil"
+	"github.com/gorilla/mux"
+	"encoding/json"
 )
  
 func main() {
@@ -17,8 +18,54 @@ func main() {
     log.Fatal(http.ListenAndServe(":8080", router))
 }
  
+
+type encryptionReq struct {
+  ID string
+  Data string
+}
+
+type encryptionResponse struct {
+	Result string
+	Key []byte 
+}
+
 func Encrypt(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Encrypting!")
+	fmt.Fprintln(w, "Encrypting!")
+	
+	body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+      http.Error(w, "can't read body", http.StatusBadRequest)
+      return
+	}
+
+	var requestData encryptionReq
+	err = json.Unmarshal(body, &requestData)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	responseData := encryptionResponse{}
+    if requestData.ID == "" {
+		responseData.Result = "ID parameter is missing!"
+	} else {
+		dataBytes := []byte(requestData.Data)
+		encryptedData, key, err := encrypt(dataBytes)
+		if err != nil {
+			log.Printf("Error when encrypting: %v", err)
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		log.Printf("Encrypted data: %v, Key Is: %v", encryptedData, key)
+
+		responseData.Result = "Data encrypted succesfully!"
+		responseData.Key = key
+	}
+
+	responseBody, err := json.Marshal(responseData)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseBody)
 }
  
 func Decrypt(w http.ResponseWriter, r *http.Request) {
